a9336a1c8a3a051d3983d1bcfe6656adafb25ed7
Tom Rix <Tom.Rix@amd.com>
2026-04-15 12:13:38 -0700

    Update some pkgdiff tests

--- fedora/rawhide/roctracer/copr-pkgdiff/test.sh
+++ fedora/rawhide/roctracer/copr-pkgdiff/test.sh
@@ -11,7 +11,108 @@ devel_a=`ls a/*-devel-*`
 devel_b=`ls b/*-devel-*`
 a=${devel_a//"-devel"/}
 b=${devel_b//"-devel"/}
+src_a=`ls a/*.src.rpm`
+src_b=`ls b/*.src.rpm`
 
+echo "== abipkgdiff == " 2>&1 | tee test.log
 abipkgdiff $a $b \
 	   --devel1 $devel_a --devel2 $devel_b \
 	   --d1 $debuginfo_a --d2 $debuginfo_b 2>&1 | tee test.log
+
+echo "" 2>&1 | tee -a test.log
+echo "== rpmlint == " 2>&1 | tee -a test.log
+cd a
+af=`basename $a`
+rpmlint $af 2>&1 > ${af}.rpmlint
+cd ..
+cd b
+bf=`basename $b`
+rpmlint $bf 2>&1 > ${bf}.rpmlint
+cd ..
+echo "diff of rpmlint for $bf" 2>&1 | tee -a test.log
+diff ${a}.rpmlint ${b}.rpmlint 2>&1 | tee -a test.log
+
+cd a
+af=`basename $devel_a`
+rpmlint $af 2>&1 > ${af}.rpmlint
+cd ..
+cd b
+bf=`basename $devel_b`
+rpmlint $bf 2>&1 > ${bf}.rpmlint
+cd ..
+echo "diff of rpmlint for $bf" 2>&1 | tee -a test.log
+diff ${devel_a}.rpmlint ${devel_b}.rpmlint 2>&1 | tee -a test.log
+
+cd a
+af=`basename $src_a`
+rpmlint $af 2>&1 > ${af}.rpmlint
+cd ..
+cd b
+bf=`basename $src_b`
+rpmlint $bf 2>&1 > ${bf}.rpmlint
+cd ..
+echo "diff of rpm lint for $bf" 2>&1 | tee -a test.log
+diff ${src_a}.rpmlint ${src_b}.rpmlint 2>&1 | tee -a test.log
+
+echo "" 2>&1 | tee -a test.log
+echo "== rpminspect == " 2>&1 | tee -a test.log
+cd a
+af=`basename $src_a`
+rpminspect -c /usr/share/rpminspect/fedora.yaml $af 2>&1 > ${af}.rpminspect
+cd ..
+cd b
+bf=`basename $src_b`
+rpminspect -c /usr/share/rpminspect/fedora.yaml $bf 2>&1 > ${bf}.rpminspect
+cd ..
+echo "diff of rpminspect for $bf" 2>&1 | tee -a test.log
+diff ${src_a}.rpminspect ${src_b}.rpminspect 2>&1 | tee -a test.log
+
+echo "" 2>&1 | tee -a test.log
+echo "== rpm -ql, manifest == " 2>&1 | tee -a test.log
+
+cd a
+af=`basename $a`
+rpm -qlp $af 2>&1 > ${af}.manifest
+cd ..
+cd b
+bf=`basename $b`
+rpm -qlp $bf 2>&1 > ${bf}.manifest
+cd ..
+echo "diff of manifest for $bf" 2>&1 | tee -a test.log
+diff ${a}.manifest ${b}.manifest 2>&1 | tee -a test.log
+
+cd a
+af=`basename $devel_a`
+rpm -qlp $af 2>&1 > ${af}.manifest
+cd ..
+cd b
+bf=`basename $devel_b`
+rpm -qlp $bf 2>&1 > ${bf}.manifest
+cd ..
+echo "diff of manifest for $bf" 2>&1 | tee -a test.log
+diff ${devel_a}.manifest ${devel_b}.manifest 2>&1 | tee -a test.log
+
+cd a
+af=`basename $src_a`
+rpm -qlp $af 2>&1 > ${af}.manifest
+cd ..
+cd b
+bf=`basename $src_b`
+rpm -qlp $bf 2>&1 > ${bf}.manifest
+cd ..
+echo "diff of rpm lint for $bf" 2>&1 | tee -a test.log
+diff ${src_a}.manifest ${src_b}.manifest 2>&1 | tee -a test.log
+
+echo "" 2>&1 | tee -a test.log
+echo "== specfile == " 2>&1 | tee -a test.log
+cd a
+af=`basename $src_a`
+sf=roctracer
+rpm2cpio $af | cpio -idmv
+cd ..
+cd b
+bf=`basename $src_b`
+rpm2cpio $bf | cpio -idmv
+cd ..
+echo "diff of specfile $bf" 2>&1 | tee -a test.log
+diff -u a/${sf}.spec b/${sf}.spec 2>&1 | tee -a test.log
